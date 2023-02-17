401adccb641fe3daaefa7fb8b38f920b40a5619f
Shan15Dev <shansai@bluewin.ch>
2023-02-17 11:00:22 +0100

    Openai resp struct has been created

--- pkg/davinci/davinci.go
+++ pkg/davinci/davinci.go
@@ -21,6 +21,24 @@ type OpenAIBody struct {
 	Presence_penalty  float32 `json:"presence_penalty"`
 }
 
+type OpenAIResp struct {
+	Id      string `json:"id"`
+	Object  string `json:"object"`
+	Created int    `json:"created"`
+	Model   string `json:"model"`
+	Choices []struct {
+		Text         string      `json:"text"`
+		Index        int         `json:"index"`
+		Logprobs     interface{} `json:"logprobs"`
+		FinishReason string      `json:"finish_reason"`
+	} `json:"choices"`
+	Usage struct {
+		PromptTokens     int `json:"prompt_tokens"`
+		CompletionTokens int `json:"completion_tokens"`
+		TotalTokens      int `json:"total_tokens"`
+	} `json:"usage"`
+}
+
 func getKey() string {
 	err := godotenv.Load(".env")
 
